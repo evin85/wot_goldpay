4d370c60039b0c6c3659ec2b848669f43bae756b
Evaldas Inciura <eviodx@yahoo.com>
2016-10-23 15:16:56 +0300

    native javascript

--- pay.js
+++ pay.js
@@ -1,4 +1,9 @@
-var data = "Salota,100|iron_ball,100|darkwind0000,100|5TEELR0N1N,100|baciukas,100|Rabbit_Terminator,100|Laimenas,100|Kovinukas,100|ziopliai,100|lazlow,100|volvo850tdi,100|tranas,100|Tadzka_LT,100|_kerber_,100|eurotrip,100|Padejas,100|Tr3zbas,200|deivisss666,200|deivas,200|Aramburu,200|Litofcas,200|Mik7,200|futurity30,200|MacBurgeris,200|Doomas,200|pavlitto83,200|BOSERONAS,200|Rafteris,200|PabadytojasLT,200|Starkis044,200|easyman111,200|onest,200|museika,300|ramuunas,300|Alchas,300|Tamosius,300|uzpakalis,300|gramas,300|astravic11,300|Vincas624,300|Jopernyjceatar,400|ciunamis,400|Bagaa,400|Rimvis78,500|Dunk3r,500|WWAARRAA,500|TODASG_indaTANK,500|trapatatai,500|deivuzas,500|KORSHUN_LT,600|slaptas_vadas,700|sriegis222,700|Finalfour333,700|Edv166nas,700|ciunas,800|Lord003_1,900|newas,1100|evio,1300|piela,1600";
+/*
+data format:
+{player1_nickname},{gold_ammount}|{player2nickname},{gold_ammount}
+*/
+
+var data = "evio,3|newas,1";
 var players = data.split("|");
 var playersSalary = {};
 players.forEach(function(element, index, array){
@@ -10,42 +15,44 @@ players.forEach(function(element, index, array){
     //console.log(element);
 });
 
-var $rows = $(".js-account-row");
+var rows = document.getElementsByClassName("js-account-row");
+
 var clicked = false;
-$(".js-check-all-accounts").parent().click(function(){
+document.getElementsByClassName("js-check-all-accounts")[0].parentElement.addEventListener("click", function () {
     if(clicked) return;
     clicked = true;
     var is_any_payment = false;
     for(var key in playersSalary){
-    	var players = playersSalary[key];    
+        var players = playersSalary[key];
         if(players){
             is_any_payment = true;
             setTimeout(function(){
-                $(".js-check-all-accounts").parent().click();
+                document.getElementsByClassName("js-check-all-accounts")[0].parentElement.click();
                 playersSalary[key] = null;
                 setTimeout(function(){
                     players.forEach(function(element, index, array){
-                        $rows.each(function(){
-                           var pName = $(this).find(".js-account-name").html().trim();
-                           if(pName == element){
-                               $(this).find(".b-checkbox").click();
-                           }
-                        });
+
+                        for(var i = 0; i<rows.length; i++){
+                            var pName = rows[i].querySelector(".js-account-name").textContent.trim();
+                            if(pName == element){
+                                rows[i].querySelector(".b-checkbox").click();
+                            }
+                        }
                     });
                     setTimeout(function(){
-                        $(".js-action-separate-treasury").click();
+                        document.getElementsByClassName("js-action-separate-treasury")[0].click();
                         setTimeout(function(){
-                            $(".js-treasury-amount").val(key);
+                            document.getElementsByClassName("js-treasury-amount")[0].value = key;
                             clicked = false;
                         }, 1000);
                     }, 1000);
-                }, 1000);                
+                }, 1000);
             }, 500);
             break;
         }
-        
+
     }
     if(!is_any_payment){
-        alert("all salary payed");
+        alert("done");
     }
-})
+});
